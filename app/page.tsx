381dff55441eaebadb617c215713c65b6e0fac31
hussu <hussu010@gmail.com>
2023-11-27 23:09:46 +0545

    refactor navbar and add hero section text

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import { Button } from "@/components/ui/button";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
       <Navbar />
-      <Button>Click me</Button>
+      <Hero />
+      <div className="absolute inset-x-0 z-50 bg-white">
+        <div className="text-center my-6">
+          <h1 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Popular Adventures
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </a>
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
