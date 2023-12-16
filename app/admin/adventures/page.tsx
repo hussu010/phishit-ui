8930468004ce759fa63d651cd8ac52f3d3aa3725
hussu <hussu010@gmail.com>
2023-12-16 21:59:43 +0545

    add blank admin page

---
+++ app/admin/adventures/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+export default function Adventures() {
+  return (
+    <div className="bg-white">
+      <Navbar />
+    </div>
+  );
+}
