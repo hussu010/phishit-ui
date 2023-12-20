688e7bb5ce9fc1a09b968c7febee6c08010373ba
hussu <hussu010@gmail.com>
2023-12-20 08:12:53 +0545

    add avatar to usernav

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -36,7 +36,10 @@ export default function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarImage
+              src={`https://api.dicebear.com/7.x/adventurer/svg?seed=${username}`}
+              alt="avatar-image"
+            />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
