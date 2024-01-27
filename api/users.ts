4460638375229346e49ae1d0ee0c72fed36c03bd
hussu010 <dhakalkushal44@gmail.com>
2024-01-27 14:58:31 +0545

    feature:/ unenrolled adventure

--- api/users.ts
+++ api/users.ts
@@ -1,5 +1,18 @@
 import { API_URL } from "@/config/constants";
 
+export interface UserProfile {
+  _id: string;
+  user: string;
+  fullName: string;
+  email: string;
+  gender: "MALE" | "FEMALE" | "OTHER"; // Assuming gender is one of these options
+  dateOfBirth: string;
+  bio: string;
+  avatarUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export const getMe = async (token: string) => {
   try {
     const response = await fetch(`${API_URL}/api/users/me`, {
@@ -28,3 +41,32 @@ export const getMe = async (token: string) => {
     throw error;
   }
 };
+
+export const profile = async (token: string) => {
+  try {
+    const response = await fetch(`${API_URL}/api/profiles`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      if (
+        (response.status === 500 || response.status === 401) &&
+        data.message
+      ) {
+        throw new Error(data.message);
+      } else {
+        throw new Error("Unknown error occurred");
+      }
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error getting current user:", error);
+    throw error;
+  }
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { UnenrollAdventure } from "@/api/adventures";
-import { getMe } from "@/api/users";
+import { Adventure, UnenrollAdventure } from "@/api/adventures";
+import { UserProfile, getMe, profile } from "@/api/users";
 import { RootState } from "@/redux/reducer";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,112 +9,84 @@ import { useSelector } from "react-redux";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function Page() {
-  const [userDetail, setUserDetail] = useState<any>();
+  const [userDetail, setUserDetail] = useState<UserProfile>();
+  const [enrollAdventure, setEnrollAdventure] = useState<Adventure[]>([]); // [adventureId
   const { accessToken } = useSelector((state: RootState) => state.auth);
-  const route = useRouter();
+  const { roles } = useSelector((state: RootState) => state.users);
+
+  async function getEnrolledAventure(accessToken: string) {
+    const response = await getMe(accessToken);
+    setEnrollAdventure(response.adventures);
+  }
+
+  async function getUserDetail(accessToken: string) {
+    const response = await profile(accessToken);
+    setUserDetail(response);
+  }
+
   useEffect(() => {
-    async function getUserDetail(accessToken: string) {
-      const response = await getMe(accessToken);
-      setUserDetail(response);
-    }
-    getUserDetail(accessToken);
+    // getUserDetail(accessToken);
+    getEnrolledAventure(accessToken);
   }, []);
 
-  function unenrollAdventure(id: string) {
-    UnenrollAdventure({ adventureId: id, accessToken: accessToken });
-    route.refresh();
+  async function unenrollAdventure(id: string) {
+    await UnenrollAdventure({ adventureId: id, accessToken: accessToken });
+    getEnrolledAventure(accessToken);
   }
   console.log(userDetail);
+
+  // State to manage user data
+  const [isEditing, setIsEditing] = useState(true);
+
   return (
-    <Tabs defaultValue="profile" className="w-full">
-      <TabsList>
-        <TabsTrigger value="profile">My Profile</TabsTrigger>
-        {userDetail && userDetail.roles.includes("GUIDE") && (
-          <TabsTrigger value="enrollAdventure">Enrolled Adventure</TabsTrigger>
-        )}
-      </TabsList>
-      <TabsContent value="enrollAdventure">
-        <div className="grid grid-cols-3 p-6">
-          {userDetail &&
-            userDetail.adventures.map((adventure: any) => {
+    <>
+      {roles.includes("GUIDE") && (
+        <>
+          <h1 className="text-[40px] font-bold">Enrolled Adventures:</h1>
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-2 p-6 w-full">
+            {enrollAdventure.map((adventure) => {
               return (
-                <div key={adventure._id} className="group h-[300px] w-[300px]">
-                  <h3 className="text-2xl">{adventure.title}</h3>
-                  <div className=" w-[200px] overflow-hidden rounded-md bg-gray-200 h-[150px] lg:aspect-none group-hover:opacity-75 relative">
-                    <Image
-                      src={adventure.imageUrl}
-                      alt={adventure.imageAlt}
-                      height={200}
-                      width={200}
-                      className=""
-                    />
-                  </div>
-                  <div className="mt-4 flex justify-between">
-                    <div>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {adventure.description.length > 200
-                          ? `${adventure.description.substring(0, 200)}...`
-                          : adventure.description}
-                      </p>
-                      <Button onClick={() => unenrollAdventure(adventure._id)}>
-                        Unenroll
-                      </Button>
-                    </div>
-                  </div>
-                </div>
+                <Card className="w-[500px]" key={adventure._id}>
+                  <Image
+                    src={adventure.imageUrl}
+                    alt={adventure.imageAlt}
+                    width={500}
+                    height={500}
+                  />
+                  <CardHeader>
+                    <CardTitle>{adventure.title}</CardTitle>
+                    <CardDescription>{adventure.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p></p>
+                  </CardContent>
+                  <CardFooter>
+                    <Button
+                      className="mt-2"
+                      onClick={() => {
+                        unenrollAdventure(adventure._id);
+                      }}
+                    >
+                      Unenroll
+                    </Button>
+                  </CardFooter>
+                </Card>
               );
             })}
-        </div>
-      </TabsContent>
-      <TabsContent value="profile">
-        {userDetail && (
-          <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md md:max-w-2xl">
-            <div className="md:flex">
-              {/* <div className="md:flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover md:w-48"
-            src="https://placekitten.com/200/300" // Replace with the actual user image URL
-            alt="User Avatar"
-          />
-        </div> */}
-              <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                  Roles:
-                </div>
-                <ul className="mt-2 leading-8">
-                  {userDetail.roles.map((role: string, index: number) => (
-                    <li key={index} className="list-disc">
-                      {role}
-                    </li>
-                  ))}
-                </ul>
-                <div className="mt-4">
-                  <p className="text-gray-600">
-                    Username: {userDetail.username}
-                  </p>
-                  <p className="text-gray-600">
-                    Phone Number: {userDetail.phoneNumber}
-                  </p>
-                  <p className="text-gray-600">
-                    Active: {userDetail.isActive ? "Yes" : "No"}
-                  </p>
-                  <p className="text-gray-600">
-                    Created At:{" "}
-                    {new Date(userDetail.createdAt).toLocaleString()}
-                  </p>
-                  <p className="text-gray-600">
-                    Updated At:{" "}
-                    {new Date(userDetail.updatedAt).toLocaleString()}
-                  </p>
-                </div>
-              </div>
-            </div>
           </div>
-        )}
-      </TabsContent>
-    </Tabs>
+        </>
+      )}
+    </>
   );
 }
 
