48216e678c5db533cf9664781dfde96b8298e254
hussu010 <dhakalkushal44@gmail.com>
2024-02-18 21:31:35 +0545

    added:/ terms-and-conditions

---
+++ app/terms-and-conditions/page.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col gap-4 md:px-24 px-5 py-5">
+      <h1 className="font-bold text-3xl">Terms and Conditions</h1>
+      <p>
+        These Terms and Conditions (&quot;Terms&quot;) govern your use of our website
+        (&quot;Website&quot;) and any related services provided by us. By accessing or
+        using our Website, you agree to be bound by these Terms. If you do not
+        agree to these Terms, please do not use our Website.
+      </p>
+      <h2 className="font-bold text-2xl">User Accounts</h2>
+      <p>
+        <b>1.1.</b> In order to use our Website and services, you may be
+        required to create an account with us. You agree to provide accurate,
+        current, and complete information during the registration process and to
+        update such information to keep it accurate, current, and complete.
+      </p>
+      <p>
+        <b>1.2.</b> You are responsible for safeguarding the password that you
+        use to access your account and for any activities or actions under your
+        password. We encourage you to use &quot;strong&quot; passwords (passwords that use
+        a combination of upper and lower case letters, numbers, and symbols)
+        with your account. You must notify us immediately upon becoming aware of
+        any breach of security or unauthorized use of your account.
+      </p>
+      <p>
+        <b>1.3.</b>We may suspend or terminate your account and/or refuse any
+        and all current or future use of the Website or any portion thereof for
+        any reason at any time.
+      </p>
+      <h2 className="font-bold text-2xl">Payments</h2>
+      <p>
+        <b>2.1.</b> Our Website may offer services or products for which you will be
+        required to make payments. By purchasing or using any such services or
+        products, you agree to pay all applicable fees as described on the
+        Website.
+      </p>
+      <p>
+       <b>2.2.</b>  We use secure third-party payment processors for transactions on
+        our Website. We do not store your credit card information on our
+        servers.
+      </p>
+      <p>
+        <b>2.3.</b> All payments are final and non-refundable, unless otherwise stated
+        in writing by us.
+      </p>
+      <h2 className="font-bold text-2xl">Booking</h2>
+      <p>
+        <b>3.1.</b> Our Website may offer booking services for various activities,
+        events, or accommodations. By booking through our Website, you agree to
+        abide by any terms and conditions set forth by the respective service
+        providers.
+      </p>
+      <p>
+        <b>3.2.</b> We do not guarantee the availability, accuracy, or quality of any
+        bookings made through our Website. We are not responsible for any
+        disputes or issues arising from such bookings.
+      </p>
+      <h2 className="font-bold text-2xl">User Content</h2>
+      <p>
+        <b>4.1.</b> Our Website may allow you to post, link, store, share, and
+        otherwise make available certain information, text, graphics, videos, or
+        other material (&quot;User Content&quot;). You are solely responsible for the User
+        Content that you post on or through the Website.
+      </p>
+      <p>
+        <b>4.2.</b> By posting User Content on our Website, you grant us the right and
+        license to use, modify, publicly perform, publicly display, reproduce,
+        and distribute such User Content on and through the Website.
+      </p>
+      <h2 className="font-bold text-2xl">Intellectual Property</h2>
+      <p>
+        <b>5.1.</b> Our Website and its original content (excluding User Content
+        provided by users), features, and functionality are and will remain the
+        exclusive property of Phishit and its licensors.
+      </p>
+      <p>
+        <b>5.2.</b> Our Website is protected by copyright, trademark, and other laws of
+        Nepal and foreign countries. Our trademarks and trade dress may not be
+        used in connection with any product or service without the prior written
+        consent of Phishit.
+      </p>
+      <h2 className="font-bold text-2xl">Limitation of Liability</h2>
+      <p>
+       <b>6.1.</b>  In no event shall Phishit, nor its directors, employees, partners,
+        agents, suppliers, or affiliates, be liable for any indirect,
+        incidental, special, consequential, or punitive damages, including
+        without limitation, loss of profits, data, use, goodwill, or other
+        intangible losses, resulting from (i) your access to or use of or
+        inability to access or use the Website; (ii) any conduct or content of
+        any third party on the Website; (iii) any content obtained from the
+        Website; and (iv) unauthorized access, use, or alteration of your
+        transmissions or content, whether based on warranty, contract, tort
+        (including negligence), or any other legal theory, whether or not we
+        have been informed of the possibility of such damage, and even if a
+        remedy set forth herein is found to have failed of its essential
+        purpose.
+      </p>
+      <h2 className="font-bold text-2xl">Governing Law</h2>
+      <p>
+        <b>7.1.</b> These Terms shall be governed and construed in accordance with the
+        laws of Nepal, without regard to its conflict of law provisions.
+      </p>
+      <h2>Changes</h2>
+      <p>
+       <b> 8.1.</b> We reserve the right, at our sole discretion, to modify or replace
+        these Terms at any time. If a revision is material, we will provide at
+        least [30 days&apos;] notice prior to any new terms taking effect. What
+        constitutes a material change will be determined at our sole discretion.
+      </p>
+      <h2 className="font-bold text-2xl">Contact Us</h2>
+      <p>
+       <b>9.1.</b>  If you have any questions about these Terms, please contact us at
+        phishit@gmail.com.
+      </p>
+      <p>
+        By using our Website, you agree to these Terms and our Privacy Policy.
+        Please read our Privacy Policy carefully to understand how we collect,
+        use, and disclose information about you.
+      </p>
+    </div>
+  );
+};
+
+export default Page;
